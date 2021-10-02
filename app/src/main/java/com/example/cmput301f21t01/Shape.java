dcb17b73a903219294ab0832366ba81d73e7b4ca
thecatinbox <catinboxx@gmail.com>
2021-10-02 00:00:30 -0600

    Lab 4 Part 1

---
+++ app/src/main/java/com/example/cmput301f21t01/Shape.java
@@ -0,0 +1,13 @@
+package com.example.cmput301f21t01;
+
+public abstract class Shape {
+
+    private int x;
+    private int y;
+
+    Shape(int _x, int _y) {
+        this.x = _x;
+        this.y = _y;
+    }
+
+}
